947a18d4dec97c876dc379b34d1eb03d134234d5
Amber Aultman <amber.aultman@gmail.com>
2015-12-21 10:41:08 -0500

    updating config for relative links fix

--- config.rb
+++ config.rb
@@ -108,6 +108,8 @@ configure :build do
   # Use relative URLs
   activate :relative_assets
 
+  set :relative_links, true
+
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
