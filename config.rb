ed2bb8d0162f5e27e15bf594bae53d6ee0d39402
Amber Aultman <amber.aultman@gmail.com>
2017-08-01 12:01:21 -0400

    Updates the config.rb file with new middleman deploy code

--- config.rb
+++ config.rb
@@ -34,9 +34,19 @@ page "articles/*", :layout => :article_layout
 page "writing/*", :layout => :writing_layout
 page "work/*", :layout => :work_layout
 
+# activate :deploy do |deploy|
+#   deploy.method = :git
+# end
+
 activate :deploy do |deploy|
-  deploy.method = :git
+  deploy.deploy_method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
 
 ###
 # Compass
